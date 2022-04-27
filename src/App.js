d31010e54208cdce727a3c6ac67ef1823f6ccef5
Eric Thomas <reactomoss@gmail.com>
2022-04-27 10:48:31 +0300

    contract plus, minus button

--- src/App.js
+++ src/App.js
@@ -21,17 +21,13 @@
  * @author samelh@google.com (Sam El-Husseini)
  */
 
-import React from 'react';
-import './App.css';
-
-import logo from './logo.svg';
-
-import BlocklyComponent, { Block, Value, Field, Shadow } from './Blockly';
-
-import BlocklyJS from 'blockly/javascript';
-
-import './blocks/customblocks';
-import './generator/generator';
+import React from "react";
+import "./App.css";
+import BlocklyComponent, { Block, Value, Field, Shadow } from "./Blockly";
+import BlocklyJS from "blockly/javascript";
+import "./blocks/customblocks";
+import "./blocks/contract";
+import "./generator/generator";
 
 class App extends React.Component {
   constructor(props) {
@@ -44,28 +40,28 @@ class App extends React.Component {
       this.simpleWorkspace.current.workspace
     );
     console.log(code);
-  }
+  };
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <button onClick={this.generateCode}>Convert</button>
-          <BlocklyComponent ref={this.simpleWorkspace}
-          readOnly={false} trashcan={true} media={'media/'}
-          move={{
-            scrollbars: true,
-            drag: true,
-            wheel: true
-          }}
-          initialXml={`
-<xml xmlns="http://www.w3.org/1999/xhtml">
-<block type="controls_ifelse" x="0" y="0"></block>
-</xml>
-      `}>
+          <BlocklyComponent
+            ref={this.simpleWorkspace}
+            readOnly={false}
+            trashcan={true}
+            media={"media/"}
+            move={{
+              scrollbars: true,
+              drag: true,
+              wheel: true,
+            }}
+          >
+            <Block type="contract" />
             <Block type="test_react_field" />
             <Block type="test_react_date_field" />
+            <Block type="lists_create_with" />
             <Block type="controls_ifelse" />
             <Block type="logic_compare" />
             <Block type="logic_operation" />
@@ -79,7 +75,7 @@ class App extends React.Component {
             <Block type="logic_operation" />
             <Block type="logic_negate" />
             <Block type="logic_boolean" />
-            <Block type="logic_null" disabled="true" />
+            <Block type="logic_null" />
             <Block type="logic_ternary" />
             <Block type="text_charAt">
               <Value name="VALUE">

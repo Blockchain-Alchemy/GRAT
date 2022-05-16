35db66e6fa2435a45a7290008c83808728c5d846
Eric Thomas <reactomoss@gmail.com>
2022-05-16 07:41:03 +0300

    fix map and bigmap

--- src/generator/python/map.js
+++ src/generator/python/map.js
@@ -3,13 +3,13 @@ import 'blockly/python';
 const Python = Blockly.Python;
 
 Blockly.Python['sp_map'] = function(block) {
-  const tkey = Python.valueToCode(block, 'KEY1', Python.ORDER_NONE) || '';
-  const tvalue = Python.valueToCode(block, 'KEY2', Python.ORDER_NONE) || '';
+  const tkey = block.getFieldValue('KEY1')
+  const tvalue = block.getFieldValue('KEY2')
   return [`sp.map(tkey = ${tkey}, tvalue = ${tvalue})`, Python.ORDER_ATOMIC];
 }
 
 Blockly.Python['sp_big_map'] = function(block) {
-  const tkey = Python.valueToCode(block, 'KEY1', Python.ORDER_NONE) || '';
-  const tvalue = Python.valueToCode(block, 'KEY2', Python.ORDER_NONE) || '';
+  const tkey = block.getFieldValue('KEY1')
+  const tvalue = block.getFieldValue('KEY2')
   return [`sp.big_map(tkey = ${tkey}, tvalue = ${tvalue})`, Python.ORDER_ATOMIC];
 }

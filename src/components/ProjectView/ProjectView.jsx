7161bb5d2bb4bde57ecd5309b9fd1a4c5433472e
Lex Dreitser <Dreitser@users.noreply.github.com>
2022-05-18 18:20:33 -0700

    Update ProjectView.jsx

--- src/components/ProjectView/ProjectView.jsx
+++ src/components/ProjectView/ProjectView.jsx
@@ -7,17 +7,18 @@ const ProjectView = (props) => {
 
   return(
     <Container className='project-view'>
-      <Title align='center' order={3}>Lessons</Title>
+      <Title align='center' order={3}>Ideas</Title>
       <Group spacing={5} style={{padding: 5}}>
-        <Text size='lg'>Staking</Text>
-        <Text size='md'>This contract will allow users to stake tezos for a certain time and then withdraw after a certain time.</Text>
+        <Text size='lg'>Buy Contract</Text>
+        <Text size='md'>This contract will allow users to buy and send it to the user. </Text>
       </Group>
       <Title order={5} style={{padding: 5}}>Follow these steps</Title>
       <Timeline active={lessonState.timeline}>
         <Timeline.Item title="Add Contract"/>
         <Timeline.Item title="Rename Contract"/>
         <Timeline.Item title="Add Entry Point"/>
-        <Timeline.Item title="Rename Entry Point to Deposit"/>
+        <Timeline.Item title="Rename Entry Point to Mint"/>
+        <Timeline.Item title="Add "/>
       </Timeline>
     </Container>
   )

648472325495e794a0f2111a634bed576e0bb5af
Eric Thomas <reactomoss@gmail.com>
2022-05-21 04:44:17 +0300

    base_url

--- src/service/index.js
+++ src/service/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-//const BASE_URL = process.env.REACT_APP_BASE_URL;
-const BASE_URL = 'http://localhost:3333';
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+//const BASE_URL = 'http://localhost:3333';
 //const BASE_URL = 'https://grat.fun';
 
 export const compile = (name, code, taqId) => {

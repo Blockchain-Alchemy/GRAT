dd5b85f99a3df28da6fb96c6ac7ab8c381ce7edc
Eric Thomas <reactomoss@gmail.com>
2022-05-26 05:38:16 +0300

    update app comp

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ProjectView from './components/ProjectView/ProjectView';
 import './generator/javsacript';
 import './generator/python';
 import Unity, { UnityContext } from 'react-unity-webgl';
-import { MantineProvider, Grid } from '@mantine/core';
+import { MantineProvider } from '@mantine/core';
 import { NotificationsProvider } from '@mantine/notifications';
 import Menu from './components/Menu/Menu';
 import Recipe from 'recipes/donate.json';
@@ -40,21 +40,19 @@ const App = () => {
     >
       <NotificationsProvider>
         <Menu />
-        <Grid>
-          <Grid.Col span={9}>
-            <Workspace
-              unityContext={unityContext}
-              loading={progression < 1.0}
-              recipes={Recipe}
-            />
-          </Grid.Col>
-          <Grid.Col span={3}>
-            <div id="Sidebar" ref={sideRef}>
-              <Unity unityContext={unityContext} />
-              <ProjectView recipes={Recipe} />
-            </div>
-          </Grid.Col>
-        </Grid>
+        <div className='workBar' >
+          <Workspace
+            unityContext={unityContext}
+            loading={progression < 1.0}
+            recipes={Recipe}
+          />
+        </div>
+        <div className='Sidebar'>
+          <div id="Sidebar" ref={sideRef}>
+            <Unity unityContext={unityContext} />
+            <ProjectView recipes={Recipe} />
+          </div>
+        </div>
       </NotificationsProvider>
     </MantineProvider>
   );
